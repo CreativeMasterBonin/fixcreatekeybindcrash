258344c43378002e32cb128b921c18ce8ea61c31
blueyMasterMods <17255831+CreativeMasterBonin@users.noreply.github.com>
2026-08-20 21:08:23 -0400

    Initial commit

---
+++ src/main/java/net/cmb/fixcreatekeybindcrash/FixCreateKeybindCrash.java
@@ -0,0 +1,13 @@
+package net.cmb.fixcreatekeybindcrash;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+
+@Mod(FixCreateKeybindCrash.MODID)
+public class FixCreateKeybindCrash {
+    public static final String MODID = "fixcreatekeybindcrash";
+    public FixCreateKeybindCrash(IEventBus modEventBus, ModContainer modContainer) {
+
+    }
+}
---
+++ src/main/java/net/cmb/fixcreatekeybindcrash/mixin/FixKeyDownCheckMix.java
@@ -0,0 +1,28 @@
+package net.cmb.fixcreatekeybindcrash.mixin;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import com.mojang.logging.LogUtils;
+import com.simibubi.create.AllKeys;
+import net.minecraft.client.Minecraft;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AllKeys.class)
+@Debug(export = true)
+public class FixKeyDownCheckMix {
+    @Inject(method = "isKeyDown",at = @At("HEAD"), cancellable = true)
+    private static void isKeyDownFix(int key, CallbackInfoReturnable<Boolean> cir){
+        if(key <= GLFW.GLFW_KEY_UNKNOWN){
+            LogUtils.getLogger().error("FixKeyDownCheckMix - isKeyDownFix: Key was unknown or unassigned, which is unhandled, skipping!");
+            cir.setReturnValue(false);
+        }
+        else{
+            cir.setReturnValue(InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), key));
+        }
+        cir.cancel();
+    }
+}
